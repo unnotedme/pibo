2a85a9f290b6f84a60c0587b9a65055487437504
ColbyB322 <ccbremner2@gmail.com>
2023-03-01 17:53:20 -0400

    Switched to MySQL2

--- app/server/server.js
+++ app/server/server.js
@@ -2,7 +2,7 @@
 //import mysql from "mysql"
 
 const express = require("express")
-const mysql = require("mysql")
+const mysql = require("mysql2")
 const app = express()
 
 app.get("/api", (req, res) => {
@@ -12,15 +12,22 @@ app.get("/api", (req, res) => {
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "", //Leaving password blank for now
-    database: ""
+    password: "", //removed 
+    database: "pibo"
 })
 
-app.get("/user_login", (req,res)=>{
-    const sel = "SELECT * FROM USER_LOGIN"
-    db.query(sel,(err,data)=>{
-        if (err) return res.json(err)
+db.connect(error => {
+    if (error) throw error;
+    console.log("Successfully connected to the database.");
+  });
+  
+
+ app.get("/user_login", (req,res)=>{
+     const sel = "SELECT * FROM USER_LOGIN"
+     db.query(sel,(err,data)=>{
+if (err) return res.json(err)
     })
-})
+ })
+
 
 app.listen(5000, () => {console.log("Server started on port 5000.")})
