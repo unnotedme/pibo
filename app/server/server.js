6811f73da680ff7c44b7905c71961b1e7bc02896
ColbyB322 <ccbremner2@gmail.com>
2023-03-02 15:53:33 -0400

    DB connection works, and can select all

--- app/server/server.js
+++ app/server/server.js
@@ -1,18 +1,12 @@
-//import express from "express"
-//import mysql from "mysql"
-
 const express = require("express")
 const mysql = require("mysql2")
 const app = express()
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo"] })
-})
 
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "", //removed 
+    password: "", //removed for now
     database: "pibo"
 })
 
@@ -20,14 +14,18 @@ db.connect(error => {
     if (error) throw error;
     console.log("Successfully connected to the database.");
   });
-  
 
- app.get("/user_login", (req,res)=>{
-     const sel = "SELECT * FROM USER_LOGIN"
-     db.query(sel,(err,data)=>{
-if (err) return res.json(err)
+
+ app.get("/user_register", (req,res)=>{
+    db.query("SELECT * FROM USER_REGISTER", (err, result) => {
+        if (err){
+            console.log("Error selecting.")
+        }
+        res.send(result);
     })
- })
+})
+
+db.end;
 
 
 app.listen(5000, () => {console.log("Server started on port 5000.")})
