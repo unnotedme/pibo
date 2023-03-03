7dd35a23648a39107e74ff6e8ff62feb1e37bc4e
ColbyB322 <ccbremner2@gmail.com>
2023-03-03 07:06:41 -0400

    Added comments.

--- app/server/server.js
+++ app/server/server.js
@@ -6,17 +6,19 @@ const app = express()
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "", //removed for now
+    password: "",   //write password here
     database: "pibo"
 })
 
-db.connect(error => {
+app.listen(5000, () => {console.log("Server started on port 5000.")})
+
+db.connect(error => { //opening connection
     if (error) throw error;
     console.log("Successfully connected to the database.");
   });
 
 
- app.get("/user_register", (req,res)=>{
+ app.get("/user_register", (req,res)=>{ //Returns records from user register
     db.query("SELECT * FROM USER_REGISTER", (err, result) => {
         if (err){
             console.log("Error selecting.")
@@ -25,7 +27,6 @@ db.connect(error => {
     })
 })
 
-db.end;
+db.end; //closing connection
 
 
-app.listen(5000, () => {console.log("Server started on port 5000.")})
